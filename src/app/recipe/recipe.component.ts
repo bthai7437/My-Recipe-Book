d1cb414229f293cde2e97af8236bf4acfdc4d16b
Steven Thai <steven_thai_8@hotmail.com>
2019-07-17 15:10:17 -0400

    added background pics for cards and flexed in the center

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeComponent implements OnInit {
 
+  cards = [
+    '\assets\Images\recipe-food-breakfast.jpg',
+    '\assets\Images\recipe-food-lunch.jpg',
+    '\assets\Images\recipe-food-dinner.jpg',
+    '\assets\Images\recipe-food-desert.jpg'
+  ];
+
+  imgSrc: string;
   constructor() { }
 
   ngOnInit() {
